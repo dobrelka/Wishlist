70ae1d840bf88822eb33619ca65f6d84de34feba
dobrelka <dobrelyatest@gmail.com>
2020-10-22 13:40:38 +0300

    Adding test saveNewItemSavesData in DetailViewModelTest.kt

--- app/src/androidTest/java/com/raywenderlich/android/wishlist/DetailViewModelTest.kt
+++ app/src/androidTest/java/com/raywenderlich/android/wishlist/DetailViewModelTest.kt
@@ -1,9 +1,11 @@
 package com.raywenderlich.android.wishlist
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.room.Room
 import androidx.test.platform.app.InstrumentationRegistry
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.raywenderlich.android.wishlist.persistance.RepositoryImpl
 import com.raywenderlich.android.wishlist.persistance.WishlistDao
@@ -33,6 +35,22 @@ class DetailViewModelTest {
         "Smart watch")
 
     verify(wishlistDao).save(any())
+  }
+
+  @Test
+  fun saveNewItemSavesData() {
+    val wishlist = Wishlist("Victoria", listOf("RW Android Apprentice Book", "Android phone"), 1)
+
+    val name = "Smart watch"
+    viewModel.saveNewItem(wishlist, name)
+
+    val mockObserver = mock<Observer<Wishlist>>()
+
+    wishlistDao.findById(wishlist.id)
+            .observeForever(mockObserver)
+    verify(mockObserver).onChanged(
+            wishlist.copy(wishes = wishlist.wishes + name)
+    )
   }
 
 }
