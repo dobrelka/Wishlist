60f86cfcb5d19e783742a069fe00fd1e34987319
dobrelka <dobrelyatest@gmail.com>
2020-10-22 11:52:39 +0300

    Adding  test saveNewItemCallsDatabase in DetailViewModelTest.kt

---
+++ app/build/generated/source/kapt/release/com/raywenderlich/android/wishlist/persistance/WishlistDatabase_Impl.java
@@ -0,0 +1,134 @@
+package com.raywenderlich.android.wishlist.persistance;
+
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomOpenHelper;
+import androidx.room.RoomOpenHelper.Delegate;
+import androidx.room.util.DBUtil;
+import androidx.room.util.TableInfo;
+import androidx.room.util.TableInfo.Column;
+import androidx.room.util.TableInfo.ForeignKey;
+import androidx.room.util.TableInfo.Index;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+import androidx.sqlite.db.SupportSQLiteOpenHelper.Callback;
+import androidx.sqlite.db.SupportSQLiteOpenHelper.Configuration;
+import java.lang.IllegalStateException;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class WishlistDatabase_Impl extends WishlistDatabase {
+  private volatile WishlistDao _wishlistDao;
+
+  @Override
+  protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration configuration) {
+    final SupportSQLiteOpenHelper.Callback _openCallback = new RoomOpenHelper(configuration, new RoomOpenHelper.Delegate(1) {
+      @Override
+      public void createAllTables(SupportSQLiteDatabase _db) {
+        _db.execSQL("CREATE TABLE IF NOT EXISTS `Wishlist` (`receiver` TEXT NOT NULL, `wishes` TEXT NOT NULL, `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL)");
+        _db.execSQL("CREATE TABLE IF NOT EXISTS room_master_table (id INTEGER PRIMARY KEY,identity_hash TEXT)");
+        _db.execSQL("INSERT OR REPLACE INTO room_master_table (id,identity_hash) VALUES(42, 'a7eb5d67ef97cf2badaead4099b54405')");
+      }
+
+      @Override
+      public void dropAllTables(SupportSQLiteDatabase _db) {
+        _db.execSQL("DROP TABLE IF EXISTS `Wishlist`");
+      }
+
+      @Override
+      protected void onCreate(SupportSQLiteDatabase _db) {
+        if (mCallbacks != null) {
+          for (int _i = 0, _size = mCallbacks.size(); _i < _size; _i++) {
+            mCallbacks.get(_i).onCreate(_db);
+          }
+        }
+      }
+
+      @Override
+      public void onOpen(SupportSQLiteDatabase _db) {
+        mDatabase = _db;
+        internalInitInvalidationTracker(_db);
+        if (mCallbacks != null) {
+          for (int _i = 0, _size = mCallbacks.size(); _i < _size; _i++) {
+            mCallbacks.get(_i).onOpen(_db);
+          }
+        }
+      }
+
+      @Override
+      public void onPreMigrate(SupportSQLiteDatabase _db) {
+        DBUtil.dropFtsSyncTriggers(_db);
+      }
+
+      @Override
+      public void onPostMigrate(SupportSQLiteDatabase _db) {
+      }
+
+      @Override
+      protected void validateMigration(SupportSQLiteDatabase _db) {
+        final HashMap<String, TableInfo.Column> _columnsWishlist = new HashMap<String, TableInfo.Column>(3);
+        _columnsWishlist.put("receiver", new TableInfo.Column("receiver", "TEXT", true, 0));
+        _columnsWishlist.put("wishes", new TableInfo.Column("wishes", "TEXT", true, 0));
+        _columnsWishlist.put("id", new TableInfo.Column("id", "INTEGER", true, 1));
+        final HashSet<TableInfo.ForeignKey> _foreignKeysWishlist = new HashSet<TableInfo.ForeignKey>(0);
+        final HashSet<TableInfo.Index> _indicesWishlist = new HashSet<TableInfo.Index>(0);
+        final TableInfo _infoWishlist = new TableInfo("Wishlist", _columnsWishlist, _foreignKeysWishlist, _indicesWishlist);
+        final TableInfo _existingWishlist = TableInfo.read(_db, "Wishlist");
+        if (! _infoWishlist.equals(_existingWishlist)) {
+          throw new IllegalStateException("Migration didn't properly handle Wishlist(com.raywenderlich.android.wishlist.Wishlist).\n"
+                  + " Expected:\n" + _infoWishlist + "\n"
+                  + " Found:\n" + _existingWishlist);
+        }
+      }
+    }, "a7eb5d67ef97cf2badaead4099b54405", "5a4da93d17b73447e239e54da72e1d87");
+    final SupportSQLiteOpenHelper.Configuration _sqliteConfig = SupportSQLiteOpenHelper.Configuration.builder(configuration.context)
+        .name(configuration.name)
+        .callback(_openCallback)
+        .build();
+    final SupportSQLiteOpenHelper _helper = configuration.sqliteOpenHelperFactory.create(_sqliteConfig);
+    return _helper;
+  }
+
+  @Override
+  protected InvalidationTracker createInvalidationTracker() {
+    final HashMap<String, String> _shadowTablesMap = new HashMap<String, String>(0);
+    HashMap<String, Set<String>> _viewTables = new HashMap<String, Set<String>>(0);
+    return new InvalidationTracker(this, _shadowTablesMap, _viewTables, "Wishlist");
+  }
+
+  @Override
+  public void clearAllTables() {
+    super.assertNotMainThread();
+    final SupportSQLiteDatabase _db = super.getOpenHelper().getWritableDatabase();
+    try {
+      super.beginTransaction();
+      _db.execSQL("DELETE FROM `Wishlist`");
+      super.setTransactionSuccessful();
+    } finally {
+      super.endTransaction();
+      _db.query("PRAGMA wal_checkpoint(FULL)").close();
+      if (!_db.inTransaction()) {
+        _db.execSQL("VACUUM");
+      }
+    }
+  }
+
+  @Override
+  public WishlistDao wishlistDao() {
+    if (_wishlistDao != null) {
+      return _wishlistDao;
+    } else {
+      synchronized(this) {
+        if(_wishlistDao == null) {
+          _wishlistDao = new WishlistDao_Impl(this);
+        }
+        return _wishlistDao;
+      }
+    }
+  }
+}
--- app/src/androidTest/java/com/raywenderlich/android/wishlist/DetailViewModelTest.kt
+++ app/src/androidTest/java/com/raywenderlich/android/wishlist/DetailViewModelTest.kt
@@ -3,10 +3,13 @@ package com.raywenderlich.android.wishlist
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.platform.app.InstrumentationRegistry
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.verify
 import com.raywenderlich.android.wishlist.persistance.RepositoryImpl
 import com.raywenderlich.android.wishlist.persistance.WishlistDao
 import com.raywenderlich.android.wishlist.persistance.WishlistDatabase
 import org.junit.Rule
+import org.junit.Test
 import org.mockito.Mockito
 
 class DetailViewModelTest {
@@ -23,5 +26,13 @@ class DetailViewModelTest {
 
   private val viewModel = DetailViewModel(
       RepositoryImpl(wishlistDao) )
+
+  @Test
+  fun saveNewItemCallsDatabase() {
+    viewModel.saveNewItem(Wishlist("Victoria", listOf("RW Android Apprentice Book", "Android phone"), 1),
+        "Smart watch")
+
+    verify(wishlistDao).save(any())
+  }
 
 }
