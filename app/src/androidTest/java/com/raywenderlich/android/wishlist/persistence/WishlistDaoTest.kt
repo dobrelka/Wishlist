b8ef5ed5333cf8bc65ea94b5aeed9b87ebbe2221
dobrelka <dobrelyatest@gmail.com>
2020-10-28 10:29:12 +0200

    Adding var wishlistDatabase and var wishlistDao in WishlistDaoTest.kt

--- app/src/androidTest/java/com/raywenderlich/android/wishlist/persistence/WishlistDaoTest.kt
+++ app/src/androidTest/java/com/raywenderlich/android/wishlist/persistence/WishlistDaoTest.kt
@@ -2,6 +2,8 @@ package com.raywenderlich.android.wishlist.persistence
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.raywenderlich.android.wishlist.persistance.WishlistDao
+import com.raywenderlich.android.wishlist.persistance.WishlistDatabase
 import org.junit.Rule
 import org.junit.runner.RunWith
 
@@ -10,4 +12,7 @@ class WishlistDaoTest {
 
   @get:Rule
   var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+  private lateinit var wishlistDatabase: WishlistDatabase
+  private lateinit var wishlistDao: WishlistDao
 }
