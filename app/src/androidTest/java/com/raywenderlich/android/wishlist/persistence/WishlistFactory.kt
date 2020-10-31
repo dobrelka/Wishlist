55231196b13608e7a5e0789d76e021336bd5223c
dobrelka <dobrelyatest@gmail.com>
2020-10-31 11:28:04 +0200

    Adding fun makeWishlist in WishlistFactory.kt

--- app/src/androidTest/java/com/raywenderlich/android/wishlist/persistence/WishlistFactory.kt
+++ app/src/androidTest/java/com/raywenderlich/android/wishlist/persistence/WishlistFactory.kt
@@ -1,9 +1,19 @@
 package com.raywenderlich.android.wishlist.persistence
 
+import com.raywenderlich.android.wishlist.Wishlist
 import java.util.*
 import java.util.concurrent.ThreadLocalRandom
 
 object WishlistFactory {
+
+  fun makeWishlist(): Wishlist {
+    return Wishlist(
+        makeRandomString(),
+        listOf(makeRandomString(), makeRandomString()) ,
+        makeRandomInt())
+
+  }
+
   private fun makeRandomString() = UUID.randomUUID().toString()
 
   private fun makeRandomInt() = ThreadLocalRandom.current().nextInt(0, 1000 + 1)
